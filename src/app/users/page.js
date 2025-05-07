9f3fad487c2145300d5efe3a58d7639474cf91af
JouuNiu <ini.yoga30@gmail.com>
2025-05-07 12:33:25 +0800

    next.js

--- src/app/users/page.js
+++ src/app/users/page.js
@@ -1,7 +1,19 @@
-export default function UsersPage() {
+export default function UserPage() {
     return (
-        <div>
-            <p>Halaman Users</p>
+        <div id="container" className="flex h-[100vh]">
+            <section id="navigation" className="bg-gray-400 p-4 w-[200px]">
+                <h1 classname="text-3xl font-bold text-center">connect</h1>
+                <div className="flex flex-col gap-2 mt-4">
+                    <button>User</button>
+                    <button>Hak akses</button>
+                    <button>Logout</button>
+                </div>
+            </section>
+            <section>
+                <input type="text" placeholder="cari user" className="p-3 w-500"/>
+                <div id="list-users"></div>
+            </section>
+            
         </div>
     );
 }
