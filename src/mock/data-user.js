0a424a056004ba6a06892a7863e517bbc241b85e
JouuNiu <ini.yoga30@gmail.com>
2025-05-28 13:29:03 +0800

    src

---
+++ src/mock/data-user.js
@@ -0,0 +1,26 @@
+export const dataUser = [
+  {
+    fullname: "Rahmat Saudi Al Fathir",
+    email: "rahmatsaudi@universitasmulua.ac.id",
+    role: "Admin",
+    status: "Aktif",
+  },
+  {
+    fullname: "Shafira",
+    email: "shafira@universitasmulua.ac.id",
+    role: "Employee",
+    status: "Suspended",
+  },
+  {
+    fullname: "Lebah Ganteng",
+    email: "lebahganteng@universitasmulua.ac.id",
+    role: "Employee",
+    status: "Suspended",
+  },
+  {
+    fullname: "lintang",
+    email: "lintang@universitasmulua.ac.id",
+    role: "Employee",
+    status: "Suspended",
+  },
+];
